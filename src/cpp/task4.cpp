7b62b62968aa4212680913be7edf0838b22527b1
Sohib04 <127374092+Sohib04@users.noreply.github.com>
2023-10-31 04:25:53 -1200

    Update task4.cpp

--- src/cpp/task4.cpp
+++ src/cpp/task4.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
 
-using namespace std;
+int main() {
+    int x;
+    std::cout << "Enter the number of strings: ";
+    std::cin >> x;
 
-int main(){
+    std::set<char> letters;
+    for (int i = 0; i < x; ++i) {
+        std::string str;
+        std::cout << "Enter string " << i + 1 << ": ";
+        std::cin >> str;
 
+        for (char ch : str) {
+            if (islower(ch)) {
+                letters.insert(ch);
+            }
+        }
+    }
 
-  return 0;
+    std::string sortedString;
+    for (char ch : letters) {
+        sortedString += ch;
+    }
+
+    std::sort(sortedString.begin(), sortedString.end());
+
+    std::cout << "Final string containing sorted letters: " << sortedString << std::endl;
+
+    return 0;
 }
